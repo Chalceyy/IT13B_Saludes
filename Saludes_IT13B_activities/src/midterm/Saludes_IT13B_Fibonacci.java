8558d2b21e9d75f8b033605b849cb49575fb35e1
Rexmar <rekkussuma05@gmail.com>
2025-04-03 11:54:01 +0800

    add

---
+++ Saludes_IT13B_activities/src/midterm/Saludes_IT13B_Fibonacci.java
@@ -0,0 +1,27 @@
+package midterm;
+
+public class Saludes_IT13B_Fibonacci {
+    public static void main(String[] args) {
+       int fibonacci = 10;
+        
+      for (int x = 0; x < fibonacci; x++) {
+       System.out.print(fibonacci (x)+ " ");
+      }
+      
+    }
+     public static int fibonacci (int x ){
+        if (x == 0){
+            return 0;
+        } else if (x == 1){
+        
+           return 1;
+           
+       } else { 
+           return  fibonacci (x - 1 ) + fibonacci (x - 2);
+       }
+  
+       
+
+}  
+}
+  
